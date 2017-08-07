8a7cac230eb33a0874ad6cae82f36dd0e4a92eb5
semihunaldi <semihunaldi@gmail.com>
2017-08-07 22:04:33 +0300

    new test added

--- src/test/java/com/semihunaldi/excelorm/ExcelWriterTest.java
+++ src/test/java/com/semihunaldi/excelorm/ExcelWriterTest.java
@@ -15,6 +15,17 @@ public class ExcelWriterTest extends BaseTest
         taskList.remove(3);
         taskList.get(2).setAge(999);
         ExcelWriter excelWriter = new ExcelWriter();
+        excelWriter.write(getTestFile("TaskExcel.xlsx"),taskList,Task.class);
+    }
+
+    @Test
+    public void testNonExisting() throws Exception
+    {
+        ExcelReader excelReader = new ExcelReader();
+        List<Task> taskList = excelReader.read(getTestFile("TaskExcel.xlsx"), Task.class);
+        taskList.remove(3);
+        taskList.get(2).setAge(999);
+        ExcelWriter excelWriter = new ExcelWriter();
         excelWriter.write(new File("TaskExcel_new.xlsx"),taskList,Task.class);
     }
 }
